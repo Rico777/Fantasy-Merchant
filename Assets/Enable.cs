7d43fc9abdb9434a7b4f126f09b13a6fd620a94b
Rico777 <rg3112@columbia.edu>
2019-12-06 13:39:26 -0800

    imported memu from test project

---
+++ Assets/Enable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; 
+public class Enable : MonoBehaviour , IPointerDownHandler 
+{
+    public Transform itemsParent; 
+    public Image redButton;
+    public Image inventoryButton;
+    public Text inventoryText; 
+    int i = 0;
+    Image[] slots; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        redButton.enabled = false;
+        inventoryButton.enabled = true;
+        inventoryText.enabled = false;
+        slots = itemsParent.GetComponentsInChildren<Image>();
+
+        while (slots[i])
+        {
+            slots[i].enabled = false;
+            i++;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        redButton.enabled = !redButton.enabled;
+        inventoryButton.enabled = !inventoryButton.enabled;
+        inventoryText.enabled = !inventoryText.enabled;
+        i = 0;
+
+        while (slots[i])
+        {
+            slots[i].enabled = !slots[i].enabled;
+            i++;
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{   
+    public float moveSpeed = .5f;
+
+    public Transform py;
+
+    Vector3 movement; 
+
+    // For applications of that input 
+
+    private void FixedUpdate()
+    {
+        py.position = py.position + (movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    
+    //For movement Update is to get input from our player
+
+    void Update()    
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.z = 0;
+    }
+}
